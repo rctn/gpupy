77411b6eabdcb1b5171b8bdcb119446a2135e947
JesseLivezey <jesse.livezey@gmail.com>
2014-05-14 11:55:13 -0700

    timing for dot, need to add others

---
+++ timing.py
@@ -0,0 +1,60 @@
+#This file will time various versions of LCA
+from __future__ import division
+import numpy as np
+from timeit import default_timer as timer
+from gpupy import Gpupy
+from numbapro import cuda
+
+
+def main():
+    """Times various versions of Matching Pursuit."""
+
+    nshort = 6
+    tshort = 2
+    nmed = 3
+    tmed = 6
+    nlong = 1
+    
+    #Setup variables for testing
+    dimMatrix = (8192,8192)
+    d_type = np.float32
+    nIter = 10
+    gp = Gpupy()
+    
+    params = """Parameters:
+             Matrix Size: """+str(dimMatrix)+"""
+             nIter: """+str(nIter)+"""\n"""
+    print params
+             
+    start = timer()
+    matrix1 = np.array(np.random.rand(*dimMatrix),dtype=d_type)
+    matrix2 = np.array(np.random.rand(*dimMatrix),dtype=d_type)
+    dt = timer()-start
+    print '---------------Numpy based dot---------------'
+    print 'Time to create arrays:'
+    print '%f s' % dt
+    start = timer()
+    for ii in xrange(nIter):
+        np.dot(matrix1,matrix2)
+    dt = timer()-start
+    print 'Time for '+str(nIter)+' dots:'
+    print '%f s' % dt
+
+    start = timer()
+    matrix1 = cuda.to_device(np.array(np.random.rand(*dimMatrix),dtype=d_type,order='F'))
+    matrix2 = cuda.to_device(np.array(np.random.rand(*dimMatrix),dtype=d_type,order='F'))
+    dt = timer()-start
+    print '-----------NumbaPro GPU based dot------------'
+    print 'Time to create arrays:'
+    print '%f s' % dt
+    start = timer()
+    for ii in xrange(nIter):
+        gp.dot(matrix1,matrix2)
+    dt = timer()-start
+    print 'Average time over '+str(nIter)+' dots:'
+    print '%f s' % dt
+    
+
+
+if __name__ == '__main__':
+    main()
