a6497b4fc2d5c9e5c4f8a924f71739d0b095fe12
JesseLivezey <jesse.livezey@gmail.com>
2014-08-19 18:05:26 -0700

    cleaned up timing

--- timing.py
+++ timing.py
@@ -15,7 +15,8 @@ tmed = 6
 nlong = 1
   
 #Setup variables for testing
-dimMatrix = (4096,4096)
+dim = 4096
+dimMatrix = (dim,dim)
 d_type = np.float32
 nIter = 10
     
@@ -27,14 +28,14 @@ print params
 start = timer()
 matrix1 = np.array(np.random.rand(*dimMatrix),dtype=d_type, order='F')
 matrix2 = np.array(np.random.rand(*dimMatrix),dtype=d_type, order='F')
-matrix3_np = np.array(np.random.rand(*dimMatrix),dtype=d_type, order='F')
+matrix3_np = np.zeros(shape=dimMatrix,dtype=d_type, order='F')
 dt = timer()-start
 print '---------------Numpy based dot---------------'
 print 'Time to create arrays:'
 print '%f s' % dt
 start = timer()
 for ii in xrange(nIter):
-    matrix3_np[:] = np.dot(matrix1,matrix2)
+    matrix3_np[:] = np.dot(matrix1, matrix2)
 dt = timer()-start
 print 'Time for '+str(nIter)+' dots:'
 print '%f s' % dt
@@ -43,14 +44,14 @@ gp = Gpupy()
 start = timer()
 matrix1 = cuda.to_device(matrix1)
 matrix2 = cuda.to_device(matrix2)
-matrix3_gp = cuda.to_device(np.array(np.random.rand(*dimMatrix),dtype=d_type,order='F'))
+matrix3_gp = cuda.to_device(np.zeros(shape=dimMatrix,dtype=d_type,order='F'))
 dt = timer()-start
 print '-----------NumbaPro GPU based dot------------'
 print 'Time to create arrays:'
 print '%f s' % dt
 start = timer()
 for ii in xrange(nIter):
-    gp.dot(matrix1,matrix2, out=matrix3_gp)
+    gp.dot(matrix1, matrix2, out=matrix3_gp)
 dt = timer()-start
 assert np.allclose(matrix3_gp.copy_to_host(), matrix3_np), "dot products not returning same answer"
 print 'Time for '+str(nIter)+' dots:'
@@ -67,16 +68,16 @@ print ''
 print params
              
 start = timer()
-matrix1 = np.array(np.random.rand(*dimMatrix),dtype=d_type, order='F')
-matrix2 = np.array(np.random.rand(*dimMatrix),dtype=d_type, order='F')
-matrix3_np = np.array(np.random.rand(*dimMatrix),dtype=d_type, order='F')
+matrix1 = np.array(np.random.rand(*dimMatrix), dtype=d_type, order='F')
+matrix2 = np.array(np.random.rand(*dimMatrix), dtype=d_type, order='F')
+matrix3_np = np.zeros(shape=dimMatrix, dtype=d_type, order='F')
 dt = timer()-start
 print '---------------Numpy based add---------------'
 print 'Time to create arrays:'
 print '%f s' % dt
 start = timer()
 for ii in xrange(nIter):
-    matrix3_np[:] = np.add(matrix1,matrix2)
+    matrix3_np[:] = np.add(matrix1, matrix2)
 dt = timer()-start
 print 'Time for '+str(nIter)+' adds:'
 print '%f s' % dt
@@ -85,14 +86,14 @@ gp = Gpupy()
 start = timer()
 matrix1 = cuda.to_device(matrix1)
 matrix2 = cuda.to_device(matrix2)
-matrix3_gp = cuda.to_device(np.array(np.random.rand(*dimMatrix),dtype=d_type,order='F'))
+matrix3_gp = cuda.to_device(np.zeros(shape=dimMatrix, dtype=d_type,order='F'))
 dt = timer()-start
 print '-----------NumbaPro GPU based add------------'
 print 'Time to create arrays:'
 print '%f s' % dt
 start = timer()
 for ii in xrange(nIter):
-    gp.add(matrix1,matrix2, out=matrix3_gp)
+    gp.add(matrix1, matrix2, out=matrix3_gp)
 dt = timer()-start
 assert np.allclose(matrix3_gp.copy_to_host(), matrix3_np), "add not returning same results"
 print 'Time for '+str(nIter)+' adds:'
