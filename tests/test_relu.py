2645d2668ab6f9cc687b8e2db6b2c099d5231a8a
JesseLivezey <jesse.livezey@gmail.com>
2014-09-05 10:35:12 -0700

    added out tests

--- tests/test_relu.py
+++ tests/test_relu.py
@@ -1,87 +1,110 @@
 from gpupy.gpupy import Gpupy
 import numpy as np
 import copy
+from numbapro import cuda
 
 class test_relu():
 
-	def setup(self):
-            self.gp = Gpupy()
-            self.rng = np.random.RandomState(0)
+    def setup(self):
+        self.gp = Gpupy()
+        self.rng = np.random.RandomState(0)
 
-	def test_relu_m(self):
-            """relu on matrices."""
-            a = self.rng.rand(129, 1025).astype(np.float32)
-            t = .5
+    def test_relu_m(self):
+        """relu on matrices."""
+        a = self.rng.rand(129, 1025).astype(np.float32)
+        t = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t] = 0.
+        out_gp = self.gp.relu(a, thresh=t).copy_to_host()
+        assert(np.allclose(out_np, out_gp))
 
-            out_np = copy.deepcopy(a)
-            out_np[out_np<t] = 0.
-            out_gp = self.gp.relu(a, thresh=t).copy_to_host()
+        a = self.rng.rand(129, 1025).astype(np.float32)
+        val = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t] = val
+        out_gp = self.gp.relu(a, thresh=t, set_val=val).copy_to_host()
+        assert(np.allclose(out_np, out_gp))
 
-            assert(np.allclose(out_np, out_gp))
+        a = self.rng.rand(129, 1025).astype(np.float32)
+        val = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t] = val
+        out_gp = cuda.to_device(a)
+        self.gp.relu(a, thresh=t, set_val=val, out=out_gp)
+        assert(np.allclose(out_np, out_gp.copy_to_host()))
 
-            a = self.rng.rand(129, 1025).astype(np.float32)
-            val = .5
-            out_np = copy.deepcopy(a)
-            out_np[out_np<t] = val
-            out_gp = self.gp.relu(a, thresh=t, set_val=val).copy_to_host()
+    def test_relu_m_v(self):
+        """relu on matrices with vectors for thresholds."""
+        a = self.rng.rand(129, 1025).astype(np.float32)
+        t = self.rng.rand(1025).astype(np.float32)
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t] = 0.
+        out_gp = self.gp.relu(a, thresh=t).copy_to_host()
+        assert(np.allclose(out_np, out_gp))
 
-            assert(np.allclose(out_np, out_gp))
+        a = self.rng.rand(129, 1025).astype(np.float32)
+        t = self.rng.rand(129).astype(np.float32)
+        val = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t] = val
+        out_gp = self.gp.relu(a, thresh=t, set_val=val).copy_to_host()
+        assert(np.allclose(out_np, out_gp))
 
-	def test_relu_m_v(self):
-            """relu on matrices with newaxis vectors for thresholds."""
-            a = self.rng.rand(129, 1025).astype(np.float32)
-            t = self.rng.rand(1025).astype(np.float32)
+        a = self.rng.rand(129, 1025).astype(np.float32)
+        t = self.rng.rand(129).astype(np.float32)
+        val = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t] = val
+        out_gp = cuda.to_device(a)
+        self.gp.relu(a, thresh=t, set_val=val, out=out_gp)
+        assert(np.allclose(out_np, out_gp.copy_to_host()))
 
-            out_np = copy.deepcopy(a)
-            out_np[out_np<t] = 0.
-            out_gp = self.gp.relu(a, thresh=t).copy_to_host()
+    def test_relu_m_v(self):
+        """relu on matrices with newaxis vectors for thresholds."""
+        a = self.rng.rand(129, 1025).astype(np.float32)
+        t = self.rng.rand(1025).astype(np.float32)
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t[np.newaxis,:]] = 0.
+        out_gp = self.gp.relu(a, thresh=t[np.newaxis,:]).copy_to_host()
+        assert(np.allclose(out_np, out_gp))
 
-            assert(np.allclose(out_np, out_gp))
+        a = self.rng.rand(129, 1025).astype(np.float32)
+        t = self.rng.rand(129).astype(np.float32)
+        val = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t[:,np.newaxis]] = val
+        out_gp = self.gp.relu(a, thresh=t[:,np.newaxis], set_val=val).copy_to_host()
+        assert(np.allclose(out_np, out_gp))
 
-            a = self.rng.rand(129, 1025).astype(np.float32)
-            t = self.rng.rand(129).astype(np.float32)
-            val = .5
-            out_np = copy.deepcopy(a)
-            out_np[out_np<t] = val
-            out_gp = self.gp.relu(a, thresh=t, set_val=val).copy_to_host()
+        a = self.rng.rand(129, 1025).astype(np.float32)
+        t = self.rng.rand(129).astype(np.float32)
+        val = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t[:,np.newaxis]] = val
+        out_gp = cuda.to_device(a)
+        self.gp.relu(a, thresh=t[:,np.newaxis], set_val=val, out=out_gp)
+        assert(np.allclose(out_np, out_gp.copy_to_host()))
 
-            assert(np.allclose(out_np, out_gp))
+    def test_relu_v(self):
+        """relu on vectors"""
+        a = self.rng.rand(129).astype(np.float32)
+        t = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t] = 0.
+        out_gp = self.gp.relu(a, thresh=t).copy_to_host()
+        assert(np.allclose(out_np, out_gp))
 
-	def test_relu_m_v(self):
-            """relu on matrices with newaxis vectors for thresholds."""
-            a = self.rng.rand(129, 1025).astype(np.float32)
-            t = self.rng.rand(1025).astype(np.float32)
+        a = self.rng.rand(129).astype(np.float32)
+        val = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t] = val
+        out_gp = self.gp.relu(a, thresh=t, set_val=val).copy_to_host()
+        assert(np.allclose(out_np, out_gp))
 
-            out_np = copy.deepcopy(a)
-            out_np[out_np<t[np.newaxis,:]] = 0.
-            out_gp = self.gp.relu(a, thresh=t[np.newaxis,:]).copy_to_host()
-
-            assert(np.allclose(out_np, out_gp))
-
-            a = self.rng.rand(129, 1025).astype(np.float32)
-            t = self.rng.rand(129).astype(np.float32)
-            val = .5
-            out_np = copy.deepcopy(a)
-            out_np[out_np<t[:,np.newaxis]] = val
-            out_gp = self.gp.relu(a, thresh=t[:,np.newaxis], set_val=val).copy_to_host()
-
-            assert(np.allclose(out_np, out_gp))
-
-	def test_relu_v(self):
-            """relu on vectors"""
-            a = self.rng.rand(129).astype(np.float32)
-            t = .5
-
-            out_np = copy.deepcopy(a)
-            out_np[out_np<t] = 0.
-            out_gp = self.gp.relu(a, thresh=t).copy_to_host()
-
-            assert(np.allclose(out_np, out_gp))
-
-            a = self.rng.rand(129).astype(np.float32)
-            val = .5
-            out_np = copy.deepcopy(a)
-            out_np[out_np<t] = val
-            out_gp = self.gp.relu(a, thresh=t, set_val=val).copy_to_host()
-
-            assert(np.allclose(out_np, out_gp))
+        a = self.rng.rand(129).astype(np.float32)
+        val = .5
+        out_np = copy.deepcopy(a)
+        out_np[out_np<t] = val
+        out_gp = cuda.to_device(a)
+        self.gp.relu(a, thresh=t, set_val=val, out=out_gp)
+        assert(np.allclose(out_np, out_gp.copy_to_host()))
--- tests/test_zeros.py
+++ tests/test_zeros.py
@@ -1,5 +1,6 @@
 from gpupy.gpupy import Gpupy
 import numpy as np
+from numbapro import cuda
 
 class test_zeros():
 
@@ -12,10 +13,16 @@ class test_zeros():
         out_gp = self.gp.zeros(shape=(129,1025)).copy_to_host()
         assert(np.allclose(out_np, out_gp))
 
+        out_np = np.zeros(shape=(129,1025), dtype=np.float32)
+        out_gp = cuda.to_device(np.ones(shape=(129,1025), dtype=np.float32))
+        self.gp.zeros(shape=(129,1025), out=out_gp)
+        assert(np.allclose(out_np, out_gp.copy_to_host()))
+
         out_np = np.zeros(shape=129, dtype=np.float32)
         out_gp = self.gp.zeros(shape=129).copy_to_host()
         assert(np.allclose(out_np, out_gp))
 
         out_np = np.zeros(129, dtype=np.float32)
-        out_gp = self.gp.zeros(129).copy_to_host()
-        assert(np.allclose(out_np, out_gp))
+        out_gp = cuda.to_device(np.ones(129, dtype=np.float32))
+        self.gp.zeros(shape=129, out=out_gp)
+        assert(np.allclose(out_np, out_gp.copy_to_host()))
